26f009438a92c1a30ced79f0d529def50d35fa56
jimlai <swift2931@gmail.com>
2020-04-10 20:59:54 +0800

    Initial Commit

---
+++ SwTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwTest2
+//
+//  Created by JimLai on 2020/4/10.
+//  Copyright © 2020 stargate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
